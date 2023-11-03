f006cfae183b753edcb552ac4cd80d4f660a8fb7
tzs919 <tzs919@163.com>
2023-11-03 16:18:59 +0800

    first commit

---
+++ tacocloud/README.md
@@ -0,0 +1,3 @@
+# Oauth2
+## mvn clean package
+## java -jar tacocloud/target/tacocloud-0.0.7-SNAPSHOT.jar
---
+++ tacocloud/tacocloud-data/src/main/java/tacos/data/OrderRepository.java
@@ -0,0 +1,17 @@
+package tacos.data;
+
+import java.util.List;
+
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.repository.CrudRepository;
+
+import tacos.TacoOrder;
+import tacos.User;
+
+public interface OrderRepository 
+         extends CrudRepository<TacoOrder, Long> {
+
+  List<TacoOrder> findByUserOrderByPlacedAtDesc(
+          User user, Pageable pageable);
+
+}
